fd0fdfef15fa835a8a5cbc6b7e694af4588356b0
Pierluigi Cau <pierluigi@github.com>
2020-04-17 10:54:58 +0100

    Minor refactor

--- index.js
+++ index.js
@@ -6,49 +6,47 @@ const chalk = require("chalk");
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-const NUM_Q_PER_CATEGORY = 1;
-
 const qFile = "./questions.json";
-
 const log = console.log;
-var q;
-var q = JSON.parse(fs.readFileSync(qFile, "utf8"));
 
-const qByCategory = groupBy(q, q => q.category);
-let response = `
-# Random 1 on 1 Questions
+function generateQuestions(numQuestions) {
+  var q;
+  var q = JSON.parse(fs.readFileSync(qFile, "utf8"));
 
-👋🏻 Hiya! This is a randomly picked selection of 1on1 questions, dived by category. Feel free to use whatever is most relevant and only if it makese sense!
+  const qByCategory = groupBy(q, q => q.category);
+  let response = `
+  # Random 1 on 1 Questions
+  
+  👋🏻 Hiya! This is a randomly picked selection of 1on1 questions, dived by category. Feel free to use whatever is most relevant and only if it makese sense!
+  
+  `;
+  each(qByCategory, (el, k) => {
+    let qIndices = [];
+    while (qIndices.length < numQuestions) {
+      // qIndices = uniq(qIndices, [random(el.length - 1)]);
+      const r = random(el.length - 1);
+      if (qIndices.indexOf(r) < 0) qIndices.push(r);
+    }
 
-`;
-each(qByCategory, (el, k) => {
-  let qIndices = [];
-  while (qIndices.length < NUM_Q_PER_CATEGORY) {
-    // qIndices = uniq(qIndices, [random(el.length - 1)]);
-    const r = random(el.length - 1);
-    if (qIndices.indexOf(r) < 0) qIndices.push(r);
-  }
+    let questions = "";
+    qIndices.forEach(id => {
+      questions += `- ${el[id].question}
+      `;
+    });
 
-  let questions = "";
-  qIndices.forEach(id => {
-    questions += `- ${el[id].question}
-    `;
+    response += `
+  ## ${chalk.bold(k)}
+  ${questions}
+  `;
   });
 
-  response += `
-## ${chalk.bold(k)}
-${questions}
-`;
-});
+  return response;
+}
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const numQuestions = parseInt(core.getInput("num-questions"));
   log(`Hello ${numQuestions}!`);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2);
-  // console.log(`The event payload: ${payload}`);
-  core.setOutput("response", response);
+  core.setOutput("response", generateQuestions(numQuestions));
 } catch (error) {
   core.setFailed(error.message);
 }
