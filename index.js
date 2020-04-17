90c45d5841bcf6834f93a685730adb49cf4f61b5
Pierluigi Cau <pierluigi@github.com>
2020-04-17 10:52:29 +0100

    Use outputs

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   // const payload = JSON.stringify(github.context.payload, undefined, 2);
   // console.log(`The event payload: ${payload}`);
-  log(response);
+  core.setOutput("response", response);
 } catch (error) {
   core.setFailed(error.message);
 }
