4684f2090528777c3ceee5846b98108abf097c9f
Pierluigi Cau <pierluigi@github.com>
2020-04-18 10:24:10 +0100

    Check label

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
+const fs = require("fs");
 const utils = require("./utils");
-console.log(utils.generateQuestions(2, 2));
+
+const payload = JSON.parse(fs.readFileSync("./payload.json", "utf8"));
+const log = console.log;
+
+log(payload.issue.labels.find((l) => l.name == "1"));
+// console.log(utils.generateQuestions(2, 2));
