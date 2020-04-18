c76194cc642d305bed4cf36a2cd1c68c4bd3624c
Pierluigi Cau <pierluigi@github.com>
2020-04-18 10:04:05 +0100

    Refactor

--- main.js
+++ main.js
@@ -1,45 +1,39 @@
 const fs = require("fs"),
   _ = require("lodash"),
-  groupBy = require("lodash/groupBy"),
-  each = require("lodash/each"),
-  random = require("lodash/random"),
-  chalk = require("chalk"),
   core = require("@actions/core"),
   github = require("@actions/github");
 
 const qFile = "./questions.json";
-
-// TODO
-// - fetch questions from original repo
+const groupBy = _.curryRight(_.groupBy);
+const groupByCategory = groupBy((q) => q.category);
 
 function generateQuestions(numCategories, numQuestions) {
   var q = JSON.parse(fs.readFileSync(qFile, "utf8"));
-  const qByCategory = groupBy(q, (q) => q.category);
-  let response = `
-# Random 1 on 1 Questions
+  const qByCategory = groupByCategory(q);
+  let response = `# Random 1 on 1 Questions
   
-👋🏻 Hiya! This is a randomly picked selection of 1 on 1 questions (from a total of ${q.length}), dived by category. Feel free to use whatever is most relevant and only if it makese sense!
-  
-`;
-  each(qByCategory, (el, k) => {
-    let qIndices = [];
-    while (qIndices.length < numQuestions) {
-      // qIndices = uniq(qIndices, [random(el.length - 1)]);
-      const r = random(el.length - 1);
-      if (qIndices.indexOf(r) < 0) qIndices.push(r);
-    }
+👋🏻 Hiya! This is a randomly picked selection of 1 on 1 questions (from a total of ${q.length}), dived by category. Feel free to use whatever is most relevant and only if it makese sense!`;
 
-    let questions = "";
-    qIndices.forEach((id) => {
-      questions += `- ${el[id].question}
-`;
-    });
+  const flattened = _.flatten(_.sampleSize(qByCategory, numCategories));
+  const randomCategories = groupByCategory(flattened);
+  const qs = _.reduce(
+    randomCategories,
+    function (memo, curr) {
+      const rq = _.sampleSize(curr, numQuestions);
+      return _.concat(memo, rq);
+    },
+    []
+  );
 
-    response += `
-## ${chalk.bold(k)}
-${questions}
-`;
-  });
+  _.reduce(
+    groupByCategory(qs),
+    (memo, q, c) => {
+      let r = memo + `\n\n## ${c}\n`;
+      r += q.map((curr) => `- ${curr.question}`).join(`\n`);
+      response += r;
+    },
+    ""
+  );
 
   return response;
 }
