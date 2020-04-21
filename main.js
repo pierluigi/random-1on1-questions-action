32803ba2d32625d3538a1a4e1eb26a141dc0b817
Pierluigi Cau <pierluigi@github.com>
2020-04-21 10:17:07 +0100

    Support optional issue number

--- main.js
+++ main.js
@@ -9,12 +9,30 @@ async function run() {
       label = core.getInput("label"),
       numQuestions = parseInt(core.getInput("num-questions"), 10),
       numCategories = parseInt(core.getInput("num-categories"), 10),
+      issueNumberInput = parseInt(core.getInput("issue-number"), 10),
       context = github.context,
-      issue = context.payload.issue,
-      issue_number = context.issue.number,
       owner = context.repo.owner,
       repo = context.repo.repo,
       client = new github.GitHub(token);
+
+    let issue = _.get(context, "payload.issue"),
+      issue_number = _.get(context, "issue.number");
+
+    if ((!issue || !issue_number) && !isNaN(issueNumberInput)) {
+      issue_number = issueNumberInput;
+      console.log(issue_number);
+
+      issue = await client.issues.get({
+        issue_number,
+        repo,
+        owner,
+      });
+    } else {
+      core.setFailed("No issue or issue number present");
+    }
+
+    console.log(issue);
+
     if (issue.labels.find((l) => l.name == label)) {
       const body = utils.generateQuestions(numCategories, numQuestions);
       core.info(body);
--- test.js
+++ test.js
@@ -1,8 +1,6 @@
 const fs = require("fs");
 const utils = require("./utils");
 
-const payload = JSON.parse(fs.readFileSync("./payload.json", "utf8"));
 const log = console.log;
-
 // log(payload.issue.labels.find((l) => l.name == "1"));
 // console.log(utils.generateQuestions(2, 2));
